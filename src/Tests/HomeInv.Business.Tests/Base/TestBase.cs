d368360d14cbccd3533c4e9a5992f4565e60b815
Çağdaş Korkut <cagdaskorkut@gmail.com>
2021-04-14 18:45:25 +0300

    initiated TDD process

--- src/Tests/HomeInv.Business.Tests/Base/TestBase.cs
+++ src/Tests/HomeInv.Business.Tests/Base/TestBase.cs
@@ -1,6 +1,81 @@
-﻿namespace HomeInv.Business.Tests.Base
+﻿using HomeInv.Persistence;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+
+namespace HomeInv.Business.Tests
 {
-    public class TestBase
+    public abstract class TestBase
     {
+        private readonly DbContextOptions<HomeInventoryDbContext> dbContextOptions;
+        protected List<string> userIds = new List<string>();
+
+        public TestBase()
+        {
+            dbContextOptions = CreateNewContextOptions();
+            SeedUserData();
+        }
+
+        protected abstract void SeedData();
+
+        private void SeedUserData()
+        {
+            var context = GetContext();
+            string userId = Guid.NewGuid().ToString();
+            userIds.Add(userId);
+            context.Users.Add(new Persistence.Dbo.User()
+            {
+                Id = userId,
+                UserName = "test1@ab.cd",
+                Email = "test1@ab.cd",
+                PasswordHash = Guid.NewGuid().ToString()
+            });
+            userId = Guid.NewGuid().ToString();
+            userIds.Add(userId);
+            context.Users.Add(new Persistence.Dbo.User()
+            {
+                Id = userId,
+                UserName = "test2@ab.cd",
+                Email = "test1@ab.cd",
+                PasswordHash = Guid.NewGuid().ToString()
+            });
+            userId = Guid.NewGuid().ToString();
+            userIds.Add(userId);
+            context.Users.Add(new Persistence.Dbo.User()
+            {
+                Id = userId,
+                UserName = "test3@ab.cd",
+                Email = "test1@ab.cd",
+                PasswordHash = Guid.NewGuid().ToString()
+            });
+            context.SaveChanges();
+
+            SeedData();
+        }
+
+        private DbContextOptions<HomeInventoryDbContext> CreateNewContextOptions()
+        {
+            var serviceProvider = new ServiceCollection()
+                .AddEntityFrameworkInMemoryDatabase()
+                .BuildServiceProvider();
+
+            var builder = new DbContextOptionsBuilder<HomeInventoryDbContext>();
+            builder.UseInMemoryDatabase(Guid.NewGuid().ToString())
+                   .UseInternalServiceProvider(serviceProvider);
+
+            return builder.Options;
+        }
+
+        //protected HomeInventoryDbContext CreateNewContext()
+        //{
+        //    DbContextOptions<HomeInventoryDbContext> options = CreateNewContextOptions();
+        //    return new HomeInventoryDbContext(options);
+        //}
+
+        protected HomeInventoryDbContext GetContext()
+        {
+            return new HomeInventoryDbContext(dbContextOptions);
+        }
     }
 }
---
+++ src/Tests/HomeInv.Business.Tests/Home/HomeTests.cs
@@ -0,0 +1,61 @@
+﻿using HomeInv.Common.Entities;
+using HomeInv.Common.Interfaces.Services;
+using System;
+using Xunit;
+
+namespace HomeInv.Business.Tests
+{
+    public class HomeTests : TestBase, IDisposable
+    {
+        public void Dispose()
+        {
+        }
+
+        protected override void SeedData()
+        {
+            var context = GetContext();
+            IHomeService homeService = new HomeService(context);
+
+            HomeEntity homeEntity = new HomeEntity()
+            {
+                Name = "seed 1",
+                Description = "seed desc 1"
+            };
+            homeService.CreateHome(homeEntity, userIds[0]);
+
+            homeEntity = new HomeEntity()
+            {
+                Name = "seed 2",
+                Description = "seed desc 2"
+            };
+            homeService.CreateHome(homeEntity, userIds[1]);
+
+            homeEntity = new HomeEntity()
+            {
+                Name = "seed 3",
+                Description = "seed desc 3"
+            };
+            homeService.CreateHome(homeEntity, userIds[2]);
+        }
+
+        [Fact]
+        public void Home_Create_Ok()
+        {
+            // arrange
+            var context = GetContext();
+            IHomeService homeService = new HomeService(context);
+            HomeEntity homeEntity = new HomeEntity()
+            {
+                Name = "test home 1",
+                Description = "test desc 1"
+            };
+
+            // act
+            var expected = homeService.CreateHome(homeEntity, userIds[0]);
+            var actual = context.Homes.Find(1);
+
+            // assert
+            Assert.Equal(expected.Name, actual.Name);
+        }
+    }
+}
