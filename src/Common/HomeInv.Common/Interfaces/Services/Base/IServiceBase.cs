98f920f6f0b33943aeec9d572c75dd3472fd4bc4
Çağdaş Korkut <cagdaskorkut@gmail.com>
2021-04-14 18:44:38 +0300

    service interfaces cleaned up

--- src/Common/HomeInv.Common/Interfaces/Services/Base/IServiceBase.cs
+++ src/Common/HomeInv.Common/Interfaces/Services/Base/IServiceBase.cs
@@ -2,6 +2,5 @@
 {
     public interface IServiceBase
     {
-        string GetCollectionName();
     }
 }
---
+++ src/Common/HomeInv.Common/Interfaces/Services/IHomeService.cs
@@ -0,0 +1,11 @@
+﻿using HomeInv.Common.Entities;
+
+namespace HomeInv.Common.Interfaces.Services
+{
+    public interface IHomeService : IServiceBase
+    {
+        //HomeEntity GetHomeById(int id);
+        //IQueryable<HomeEntity> GetHomesOfUser(string userId);
+        HomeEntity CreateHome(HomeEntity homeEntity, string userId);
+    }
+}
