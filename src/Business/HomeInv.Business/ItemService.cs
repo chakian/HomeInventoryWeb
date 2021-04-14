6b321ac975b11d3a612437402e5b154c4b6b3b87
Çağdaş Korkut <cagdaskorkut@gmail.com>
2021-04-14 18:43:51 +0300

    ItemService is modified

--- src/Business/HomeInv.Business/ItemService.cs
+++ src/Business/HomeInv.Business/ItemService.cs
@@ -2,6 +2,7 @@
 using HomeInv.Common.Entities;
 using HomeInv.Common.Interfaces.Services;
 using HomeInv.Common.Settings;
+using HomeInv.Persistence;
 using HomeInv.Persistence.Dbo;
 using Microsoft.Extensions.Options;
 using System;
@@ -11,21 +12,9 @@ namespace HomeInv.Business
 {
     public class ItemService : ServiceBase, IItemService
     {
-        public ItemService(IOptions<CosmosSettings> _cosmosSettings)// : base(_cosmosSettings)
+        public ItemService(HomeInventoryDbContext _context) : base(_context)
         {
         }
-
-        public override string GetCollectionName() => "Items";
-
-        //public string CreateItem(Item NewItem)
-        //{
-        //    ItemDbo itemDbo = new ItemDbo();
-        //    string NewID = Guid.NewGuid().ToString();
-        //    itemDbo.Id = NewID;
-
-        //    CreateDocument<ItemDbo>(itemDbo).Wait();
-        //    return NewID;
-        //}
 
         public void DeleteItem(string id) => throw new NotImplementedException();
         public void EditItem(string id, Common.Entities.Item editedItem) => throw new NotImplementedException();
