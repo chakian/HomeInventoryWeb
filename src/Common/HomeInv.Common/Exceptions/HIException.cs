3504d1093d1493d287c89cd8b0254d33d5761d57
Çağdaş Korkut <cagdaskorkut@gmail.com>
2021-04-15 20:53:01 +0300

    modified HIException

---
+++ src/Common/HomeInv.Common/Exceptions/HIException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HomeInv.Common.Exceptions
+{
+    public class HIException : Exception
+    {
+        public HIException() : this("Bir hata oluştu") { }
+        
+        public HIException(string message) : this(message, null) { }
+        
+        public HIException(string message, Exception innerException) : base($"Bir hata oluştu. {message}", innerException) { }
+    }
+}
