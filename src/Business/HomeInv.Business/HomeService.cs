a0d983c1b627456074671df62bb2a58683ac7caa
Çağdaş Korkut <cagdaskorkut@gmail.com>
2021-04-15 17:35:05 +0300

    added GetHomesOfUser service method

--- src/Business/HomeInv.Business/HomeService.cs
+++ src/Business/HomeInv.Business/HomeService.cs
@@ -3,6 +3,9 @@ using HomeInv.Common.Entities;
 using HomeInv.Common.Interfaces.Services;
 using HomeInv.Persistence;
 using HomeInv.Persistence.Dbo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace HomeInv.Business
 {
@@ -23,6 +26,33 @@ namespace HomeInv.Business
             
             homeEntity.Id = home.Id;
             return homeEntity;
+        }
+
+        private IQueryable<Home> GetHomesOfUserInternal(string userId)
+        {
+            var homes = context.Homes.Where(q => q.InsertUserId == userId);
+            return homes;
+        }
+
+        private HomeEntity ConvertHomeToHomeEntity(Home home)
+        {
+            return new HomeEntity()
+            {
+                Id = home.Id,
+                Name = home.Name,
+                Description = home.Description
+            };
+        }
+
+        public List<HomeEntity> GetHomesOfUser(string userId)
+        {
+            var homes = GetHomesOfUserInternal(userId);
+            List<HomeEntity> homeEntities = new List<HomeEntity>();
+            foreach (var home in homes)
+            {
+                homeEntities.Add(ConvertHomeToHomeEntity(home));
+            }
+            return homeEntities;
         }
     }
 }
--- src/Common/HomeInv.Common/Interfaces/Services/IHomeService.cs
+++ src/Common/HomeInv.Common/Interfaces/Services/IHomeService.cs
@@ -1,11 +1,13 @@
 ﻿using HomeInv.Common.Entities;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace HomeInv.Common.Interfaces.Services
 {
     public interface IHomeService : IServiceBase
     {
         //HomeEntity GetHomeById(int id);
-        //IQueryable<HomeEntity> GetHomesOfUser(string userId);
         HomeEntity CreateHome(HomeEntity homeEntity, string userId);
+        List<HomeEntity> GetHomesOfUser(string userId);
     }
 }
