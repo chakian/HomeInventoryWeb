97409e7e14e5e699eba040ba7c347f4f4050c6a9
Çağdaş Korkut <cagdaskorkut@gmail.com>
2021-07-12 17:56:59 +0300

    db context modifications for auditable objects

---
+++ src/Business/HomeInv.Persistence/Configuration/AuditableEntityConfiguration.cs
@@ -0,0 +1,33 @@
+﻿using HomeInv.Persistence.Dbo;
+using Microsoft.EntityFrameworkCore;
+
+namespace HomeInv.Persistence
+{
+    public class AuditableEntityConfiguration
+    {
+        public static void Configure(ModelBuilder modelBuilder)
+        {
+            Configure<Home>(modelBuilder);
+            Configure<HomeUser>(modelBuilder);
+            //Configure<Item>(modelBuilder);
+            //Configure<ItemStock>(modelBuilder);
+            //Configure<Area>(modelBuilder);
+        }
+
+        private static void Configure<T>(ModelBuilder modelBuilder)
+            where T : BaseAuditableDbo
+        {
+            modelBuilder.Entity<T>()
+                .HasOne(s => s.InsertUser)
+                .WithMany()
+                .HasForeignKey(e => e.InsertUserId)
+                .OnDelete(DeleteBehavior.NoAction);
+
+            modelBuilder.Entity<T>()
+                .HasOne(s => s.UpdateUser)
+                .WithMany()
+                .HasForeignKey(e => e.UpdateUserId)
+                .OnDelete(DeleteBehavior.NoAction);
+        }
+    }
+}
--- src/Business/HomeInv.Persistence/HomeInventoryDbContext.cs
+++ src/Business/HomeInv.Persistence/HomeInventoryDbContext.cs
@@ -13,7 +13,53 @@ namespace HomeInv.Persistence
 
         public virtual DbSet<Home> Homes { get; set; }
         public virtual DbSet<HomeUser> HomeUsers { get; set; }
-        public virtual DbSet<Item> Items { get; set; }
-        public virtual DbSet<ItemStock> ItemStocks { get; set; }
+        //public virtual DbSet<Area> Areas { get; set; }
+        //public virtual DbSet<Item> Items { get; set; }
+        //public virtual DbSet<ItemStock> ItemStocks { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            AuditableEntityConfiguration.Configure(builder);
+
+            //builder.Entity<Area>()
+            //    .HasOne(area => area.Home)
+            //    .WithMany(home => home.Areas)
+            //    .IsRequired();
+
+            //builder.Entity<Category>()
+            //    .HasOne(category => category.ParentCategory)
+            //    .WithMany(parent => parent.ChildCategories)
+            //    .IsRequired(false);
+
+            //builder.Entity<Item>()
+            //    .HasOne(item => item.Category)
+            //    .WithMany(category => category.Items)
+            //    .IsRequired();
+            
+            //builder.Entity<ItemStock>()
+            //    .HasOne(item => item.Area)
+            //    .WithMany(area => area.ItemStocks)
+            //    .IsRequired();
+
+            //builder.Entity<ItemStock>()
+            //    .HasOne(item => item.Container)
+            //    .WithMany(container => container.ContainingItems)
+            //    .IsRequired(false);
+
+            //builder.Entity<Transaction>()
+            //    .Property(b => b.Amount)
+            //    .HasColumnType("money");
+
+            //builder.Entity<Transaction>()
+            //    .HasOne(t => t.Budget)
+            //    .WithMany(b => b.Transactions)
+            //    .OnDelete(DeleteBehavior.NoAction);
+
+            //builder.Entity<Account>()
+            //    .Property(b => b.Balance)
+            //    .HasColumnType("money");
+        }
     }
 }
