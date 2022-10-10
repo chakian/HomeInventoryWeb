b1d554a0cb27c5ecd2e403e1c900d49b9fa9b534
Çağdaş Korkut <cagdaskorkut@gmail.com>
2022-10-10 19:14:50 +0300

    Added DataCleanup class

---
+++ src/Business/HomeInv.Persistence/ManualAlteration/DataCleanup.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace HomeInv.Persistence
+{
+    public class DataCleanup
+    {
+        public void DeleteAllData(HomeInventoryDbContext dbContext)
+        {
+            dbContext.Database.ExecuteSqlRaw("DELETE FROM ItemStocks");
+            dbContext.Database.ExecuteSqlRaw("DELETE FROM Items");
+            dbContext.Database.ExecuteSqlRaw("DELETE FROM Categories");
+            dbContext.Database.ExecuteSqlRaw("DELETE FROM HomeUsers");
+            dbContext.Database.ExecuteSqlRaw("DELETE FROM Homes");
+            dbContext.Database.ExecuteSqlRaw("DELETE FROM AreaUsers");
+            dbContext.Database.ExecuteSqlRaw("DELETE FROM Areas");
+            dbContext.Database.ExecuteSqlRaw("DELETE FROM AspNetUsers");
+        }
+    }
+}
