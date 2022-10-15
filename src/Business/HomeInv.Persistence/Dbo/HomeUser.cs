af2e935ae11b10fa3a659847c306e6321a3debb0
Çağdaş Korkut <cagdaskorkut@gmail.com>
2022-10-15 09:59:33 +0300

    Added requestedby column to HomeUser table

--- src/Business/HomeInv.Persistence/Dbo/HomeUser.cs
+++ src/Business/HomeInv.Persistence/Dbo/HomeUser.cs
@@ -10,8 +10,10 @@ namespace HomeInv.Persistence.Dbo
         public string UserId { get; set; }
         [Required]
         public string Role { get; set; }
+        public string RequestedByUserId { get; set; }
 
         public virtual Home Home { get; set; }
         public virtual User User { get; set; }
+        public virtual User RequestedByUser { get; set; }
     }
 }
