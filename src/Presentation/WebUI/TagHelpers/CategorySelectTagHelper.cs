77b53577bdc4592c755af766df068ea7e5ef787e
Çağdaş Korkut <cagdaskorkut@gmail.com>
2022-11-03 16:29:26 +0300

    Upgrade to bootstrap 5. Added popper and bootstrap-select plugins

---
+++ src/Presentation/WebUI/TagHelpers/CategorySelectTagHelper.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Razor.Runtime.TagHelpers;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace WebUI.TagHelpers
+{
+    [HtmlTargetElement("category-select")]
+    public class CategorySelectTagHelper : TagHelper
+    {
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+
+        }
+    }
+}
