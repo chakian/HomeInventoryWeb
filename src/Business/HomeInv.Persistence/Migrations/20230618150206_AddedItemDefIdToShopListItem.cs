d07aa7b971be40436bfd19ff8a6dd335366b5e96
Çağdaş Korkut <cagdaskorkut@gmail.com>
2023-06-18 18:06:08 +0300

    Added ItemDefinitionId into ShoppingListItem table

---
+++ src/Business/HomeInv.Persistence/Migrations/20230618150206_AddedItemDefIdToShopListItem.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HomeInv.Persistence.Migrations
+{
+    public partial class AddedItemDefIdToShopListItem : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ItemDefinitionId",
+                table: "ShoppingListItems",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ShoppingListItems_ItemDefinitionId",
+                table: "ShoppingListItems",
+                column: "ItemDefinitionId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ShoppingListItems_ItemDefinitions_ItemDefinitionId",
+                table: "ShoppingListItems",
+                column: "ItemDefinitionId",
+                principalTable: "ItemDefinitions",
+                principalColumn: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ShoppingListItems_ItemDefinitions_ItemDefinitionId",
+                table: "ShoppingListItems");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ShoppingListItems_ItemDefinitionId",
+                table: "ShoppingListItems");
+
+            migrationBuilder.DropColumn(
+                name: "ItemDefinitionId",
+                table: "ShoppingListItems");
+        }
+    }
+}
