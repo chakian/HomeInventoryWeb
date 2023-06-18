aca43617bee2459f1853f220e9673dab5d529f44
Çağdaş Korkut <cagdaskorkut@gmail.com>
2023-06-18 21:37:30 +0300

    Add and remove items in shopping lists is done

--- src/Business/HomeInv.Persistence/Dbo/ShoppingListItem.cs
+++ src/Business/HomeInv.Persistence/Dbo/ShoppingListItem.cs
@@ -13,7 +13,7 @@ public class ShoppingListItem : BaseAuditableDbo
     public decimal Amount { get; set; }
 
     #region Size Properties
-    public int SizeUnitId { get; set; }
+    public int? SizeUnitId { get; set; }
     public virtual SizeUnit SizeUnit { get; set; }
     #endregion
 }
